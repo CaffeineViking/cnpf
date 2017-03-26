1c9e1b7f72fb8d379a6f2653a08e6a7b8ae98bfb
sciion <martin.estgren@gmail.com>
2017-03-26 16:19:22 +0200

    added unit test for GLEW and GLFW init

--- tests/main.cc
+++ tests/main.cc
@@ -2,3 +2,46 @@
 #include "foreign/catch.hpp"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#define ASSERT( expression ) std::cout<<( (expression) ? "OK" : "NOT OK" )<<std::endl;
+
+
+bool checkInitGLFW(){
+
+    const int MAJOR_VERSION = 4;
+    const int MINOR_VERSION = 3;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, MAJOR_VERSION);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, MINOR_VERSION);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(200, 200, "Default", nullptr, nullptr);    
+    if (window == nullptr)
+    {
+        glfwTerminate();
+        return false;
+    }
+    glfwMakeContextCurrent(window);
+
+    return true;
+}
+
+bool checkInitGLEW(){
+
+	glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        return false;
+    }    
+    return true;
+}
+
+TEST_CASE("Check GLFW init") {
+	REQUIRE( checkInitGLFW() == true);
+}
+
+TEST_CASE("Check GLEW init") {
+	REQUIRE( checkInitGLEW() == true);
+}
