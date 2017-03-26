c740c703a4d8473a19ebce16c43bdde951bbd75d
Martin Estgren <sci10n@users.noreply.github.com>
2017-03-26 15:37:34 +0200

    Update compile_glew.bat

--- misc/compile_glew.bat
+++ misc/compile_glew.bat
@@ -1,3 +1,4 @@
+rem Provided courtesy of https://bruceoutdoors.wordpress.com/2014/07/16/glew-for-mingw-in-windows/
 mkdir lib
 gcc -DGLEW_NO_GLU -O2 -Wall -W -Iinclude  -DGLEW_BUILD -o src/glew.o -c src/glew.c
 gcc -shared -nostdlib -Wl,-soname,libglew32.dll -Wl,--out-implib,lib/libglew32.dll.a    -o lib/glew32.dll src/glew.o -L/mingw/lib -lglu32 -lopengl32 -lgdi32 -luser32 -lkernel32
