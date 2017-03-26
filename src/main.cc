134ecbce856c3bac2de02768b6fe7f969a2368c2
Erik S. V. Jansson <caffeineviking@gmail.com>
2017-03-26 11:34:41 +0200

    Added example for cross-platform testing.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,43 @@
-#include <iostream>
-int main(int, char**) {
-    std::cout << "Hello, world!"
-              << std::endl;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Import all available OpenGL extensions. */
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+        return -1;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
