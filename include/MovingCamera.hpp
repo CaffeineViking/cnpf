f1f2412d97fa7e9bf7c9bed2822829368c627dd1
Erik S. V. Jansson <caffeineviking@gmail.com>
2017-04-25 23:53:47 +0200

    Corrected indentation and interfaces.

--- include/MovingCamera.hpp
+++ include/MovingCamera.hpp
@@ -10,27 +10,28 @@
 
 class MovingCamera {
 private:
-  glm::mat4 _projection;
-  Transform* _transform;
-  float _width;
-  float _height;
-  bool _mouseLocked;
-  glm::vec2 _lastMousePos;
-  const float SPEED = 1.0f;
-  const float SPEED_MOD = 3.0f;
+    glm::mat4 _projection;
+    Transform* _transform;
+    float _width;
+    float _height;
+    bool _mouseLocked;
+    glm::vec2 _lastMousePos;
+    const float SPEED = 1.0f;
+    const float SPEED_MOD = 3.0f;
 public:
-  MovingCamera() = delete;
-  ~MovingCamera();
+    MovingCamera() = delete;
+    ~MovingCamera();
 
-  MovingCamera(const float& fov, const float& width, const float& height);
+    MovingCamera(const float& fov, const float& width, const float& height);
 
-  void handleInput(const float& delta);
-  void update(const GLuint& programId);
-  
-  Transform* getTransform();
-  glm::mat4 getViewProjection();
-  glm::mat4 getView();
-  glm::mat4 getMVP();
+    void handleInput(const float& delta);
+    void update(const GLuint& programId) const;
 
+    Transform* getTransform();
+    const Transform* getTransform() const;
+    glm::mat4 getViewProjection() const;
+    glm::mat4 getView() const;
+    glm::mat4 getMVP() const;
 };
+
 #endif // MOVING_CAMERA_HPP
--- src/MovingCamera.cpp
+++ src/MovingCamera.cpp
@@ -5,125 +5,125 @@
 #include <cassert>
 
 MovingCamera::MovingCamera(const float& fov, const float& width, const float& height)
-  : _width(width), _height(height), _mouseLocked(false)
+    : _width(width), _height(height), _mouseLocked(false)
 {
-  _projection = glm::perspective(fov,
-				 (float)width/(float)height,
-				 0.1f,
-				 2200.0f);
-  _transform = new Transform();
+    _projection = glm::perspective(fov,
+            (float)width/(float)height,
+            0.1f,
+            2200.0f);
+    _transform = new Transform();
 }
 
 MovingCamera::~MovingCamera(){
-  delete _transform;
+    delete _transform;
 }
 
 void MovingCamera::handleInput(const float& delta){
-  float SENS = 0.001f;
-  int lockMouseKey = GLFW_MOUSE_BUTTON_LEFT;
-  
-  GLFWInputLocator* input = dynamic_cast<GLFWInputLocator*>(Locator::input());
-  assert(input != nullptr);
-  GLFWwindow* window = input->getWindow();
-  
-  //============================
-  // Rotate camera
-  //============================
-  if(!_mouseLocked && input->isButtonPressed(lockMouseKey))
+    float SENS = 0.001f;
+    int lockMouseKey = GLFW_MOUSE_BUTTON_LEFT;
+
+    GLFWInputLocator* input = dynamic_cast<GLFWInputLocator*>(Locator::input());
+    assert(input != nullptr);
+    GLFWwindow* window = input->getWindow();
+
+    //============================
+    // Rotate camera
+    //============================
+    if(!_mouseLocked && input->isButtonPressed(lockMouseKey))
     {
-      _mouseLocked = true;
-      _lastMousePos = input->getMousePos();
-      if(window)
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        _mouseLocked = true;
+        _lastMousePos = input->getMousePos();
+        if(window)
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     }
 
-  if(!input->isButtonPressed(lockMouseKey))
+    if(!input->isButtonPressed(lockMouseKey))
     {
-      _mouseLocked = false;
-      if(window)
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        _mouseLocked = false;
+        if(window)
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     }
-  
-  if(_mouseLocked){
-    glm::vec2 mousePos = input->getMousePos();
-    
-    float rotX = mousePos.x - _lastMousePos.x;
-    float rotY = mousePos.y - _lastMousePos.y;
-    
-    _lastMousePos = input->getMousePos();
 
-    // Rotate around local x-axis (vector pointing to the right of the object)
-    if(rotY != 0)
-      getTransform()->rotate(rotY * SENS, getTransform()->getRight());
+    if(_mouseLocked){
+        glm::vec2 mousePos = input->getMousePos();
 
-    // Rotate around y-axis
-    if(rotX != 0)
-      getTransform()->rotate((-rotX) * SENS, glm::vec3(0,1,0));
-  }
+        float rotX = mousePos.x - _lastMousePos.x;
+        float rotY = mousePos.y - _lastMousePos.y;
 
-  //============================
-  // Move camera
-  //============================
-  glm::vec3 translation(0.0f);
-  float amount = delta * SPEED;
-  
-  if(input->isKeyPressed(GLFW_KEY_W))
-    translation += getTransform()->getForward() * amount;
+        _lastMousePos = input->getMousePos();
 
-  if(input->isKeyPressed(GLFW_KEY_S))
-    translation += getTransform()->getBackward() * amount;
-  
-  if(input->isKeyPressed(GLFW_KEY_A))
-    translation += getTransform()->getRight() * amount;
-  
-  if(input->isKeyPressed(GLFW_KEY_D))
-    translation += getTransform()->getLeft() * amount;
-  
-  if(input->isKeyPressed(GLFW_KEY_SPACE))
-    translation += getTransform()->getUp() * amount;
-  
-  if(input->isKeyPressed(GLFW_KEY_LEFT_CONTROL))
-    translation += getTransform()->getDown() * amount;
+        // Rotate around local x-axis (vector pointing to the right of the object)
+        if(rotY != 0)
+            getTransform()->rotate(rotY * SENS, getTransform()->getRight());
 
-  if(input->isKeyPressed(GLFW_KEY_LEFT_SHIFT))
-    translation *= SPEED_MOD;
-  
-  getTransform()->translate(translation);
+        // Rotate around y-axis
+        if(rotX != 0)
+            getTransform()->rotate((-rotX) * SENS, glm::vec3(0,1,0));
+    }
+
+    //============================
+    // Move camera
+    //============================
+    glm::vec3 translation(0.0f);
+    float amount = delta * SPEED;
+
+    if(input->isKeyPressed(GLFW_KEY_W))
+        translation += getTransform()->getForward() * amount;
+
+    if(input->isKeyPressed(GLFW_KEY_S))
+        translation += getTransform()->getBackward() * amount;
+
+    if(input->isKeyPressed(GLFW_KEY_A))
+        translation += getTransform()->getRight() * amount;
+
+    if(input->isKeyPressed(GLFW_KEY_D))
+        translation += getTransform()->getLeft() * amount;
+
+    if(input->isKeyPressed(GLFW_KEY_SPACE))
+        translation += getTransform()->getUp() * amount;
+
+    if(input->isKeyPressed(GLFW_KEY_LEFT_CONTROL))
+        translation += getTransform()->getDown() * amount;
+
+    if(input->isKeyPressed(GLFW_KEY_LEFT_SHIFT))
+        translation *= SPEED_MOD;
+
+    getTransform()->translate(translation);
 }
 
 Transform* MovingCamera::getTransform(){
-  return _transform;
+    return _transform;
 }
 
-glm::mat4 MovingCamera::getViewProjection(){
-  glm::vec3 eye = _transform->getPos();
-  glm::vec3 center = _transform->getPos() + _transform->getForward();
-  glm::vec3 up = _transform->getUp();
-  return _projection * glm::lookAt(eye, center, up);
+const Transform* MovingCamera::getTransform() const {
+    return _transform;
 }
 
-glm::mat4 MovingCamera::getView(){
-  glm::vec3 eye = _transform->getPos();
-  glm::vec3 center = _transform->getPos() + _transform->getForward();
-  glm::vec3 up = _transform->getUp();
-  return glm::lookAt(eye, center, up);
+glm::mat4 MovingCamera::getViewProjection() const {
+    glm::vec3 eye = _transform->getPos();
+    glm::vec3 center = _transform->getPos() + _transform->getForward();
+    glm::vec3 up = _transform->getUp();
+    return _projection * glm::lookAt(eye, center, up);
 }
 
-glm::mat4 MovingCamera::getMVP() {
-  return getViewProjection() * getTransform()->getModel();;
+glm::mat4 MovingCamera::getView() const {
+    glm::vec3 eye = _transform->getPos();
+    glm::vec3 center = _transform->getPos() + _transform->getForward();
+    glm::vec3 up = _transform->getUp();
+    return glm::lookAt(eye, center, up);
 }
 
-void MovingCamera::update(const GLuint& programId){
-  // glm::mat4 vp = getViewProjection();
-  // GLuint vpId = glGetUniformLocation(programId, "viewProjection");
-  // glUniformMatrix4fv(vpId, 1, GL_FALSE, &(vp[0][0]));
+glm::mat4 MovingCamera::getMVP() const {
+    return getViewProjection() * getTransform()->getModel();;
+}
 
-  glm::mat4 v = getView();
-  GLuint vId = glGetUniformLocation(programId, "view");
-  glUniformMatrix4fv(vId, 1, GL_FALSE, &(v[0][0]));
+void MovingCamera::update(const GLuint& programId) const{
+    glm::mat4 v = getView();
+    GLuint vId = glGetUniformLocation(programId, "view");
+    glUniformMatrix4fv(vId, 1, GL_FALSE, &(v[0][0]));
 
-  glm::mat4 p = _projection;
-  GLuint pId = glGetUniformLocation(programId, "projection");
-  glUniformMatrix4fv(pId, 1, GL_FALSE, &(p[0][0]));
-  
+    glm::mat4 p = _projection;
+    GLuint pId = glGetUniformLocation(programId, "projection");
+    glUniformMatrix4fv(pId, 1, GL_FALSE, &(p[0][0]));
+
 }
